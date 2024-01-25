e7007c1f972a181f5da8947cc76975f943b19237
Alok Kuchlous <alokk@mirafra.com>
2024-01-25 17:14:46 +0530

    Support for labels in assembler

--- scripts/assemble.py
+++ scripts/assemble.py
@@ -30,7 +30,6 @@ for i in range(len(instruction_names)):
     instruction_opcode_map[instruction_names[i]] = i + 1
 
 def get_machine_code(instruction, tokens):
-    print(instruction)
     opcode = instruction_opcode_map[instruction]
     machine_code = 0
     # opcode is 6 bits
@@ -68,13 +67,88 @@ def check_args(instruction, tokens, line_num):
                 print("Error: invalid argument \"" + tokens[i+1] + "\"" + " on line " + str(line_num))
                 sys.exit()
         elif args[i] == "ADDR":
-            if not re.match(r"[0-9]+", tokens[i+1]):
+            # match a hex number
+            if not re.match(r"0x[0-9|a-f|A-F]+", tokens[i+1]):
                 print("Error: invalid argument \"" + tokens[i+1] + "\"" + " on line " + str(line_num))
                 sys.exit()
         elif args[i] == "IMM":
-            if not re.match(r"[0-9]+", tokens[i+1]):
+            if not re.match(r"0x[0-9|a-f|A-F]+", tokens[i+1]):
                 print("Error: invalid argument \"" + tokens[i+1] + "\"" + " on line " + str(line_num))
                 sys.exit()
+
+def tokenise(line):
+    tokens = []
+    # Everything after # is a commnt
+    line = line.split("#")[0]
+    # remove leading and trailing whitespace
+    line = line.strip()
+    # split line into tokens
+    tokens = line.split()
+    return tokens
+ 
+
+labels = {}
+def collect_labels(lines):
+    line_num = 0
+    for line in lines:
+        tokens = tokenise(line)
+        if len(tokens) == 0:
+            continue
+
+        # check for label
+        if tokens[0][-1] == ":":
+            label = tokens[0][:-1]
+            if label in labels:
+                print("Error: duplicate label \"" + label + "\"" + " on line " + str(line_num))
+                sys.exit()
+            labels[label] = line_num
+        line_num += 1
+
+def replace_labels(lines):
+    line_num = 0
+    for line in lines:
+        tokens = tokenise(line)
+        if len(tokens) > 0:
+            # Remove label from before instruction
+            if tokens[0][-1] == ":":
+                tokens = tokens[1:]
+
+            # check for label in arguments
+            for i in range(len(tokens)):
+                if tokens[i] in labels:
+                    tokens[i] = hex(labels[tokens[i]])
+            line = " ".join(tokens)
+            lines[line_num] = line
+        line_num += 1
+
+def generate_code(lines, outfile):
+    line_num = 0
+    for line in lines:
+        print("Processing line: " + line)
+        tokens = tokenise(line)
+        if len(tokens) == 0:
+            continue
+    
+        # Remove label from before instruction
+        if tokens[0][-1] == ":":
+            tokens = tokens[1:]
+    
+        # check for instruction
+        instruction = tokens[0]
+        if instruction not in instruction_names:
+            print("Error: invalid instruction \"" + instruction + "\"" + " on line " + str(line_num))
+            sys.exit()
+    
+        line_num += 1
+    
+        check_args(instruction, tokens, line_num)
+        # Create machine code from instruction
+        code = get_machine_code(instruction, tokens)
+        # write instruction opcode to output file in hex of width 8
+        outfile.write('0x'+hex(code)[2:].zfill(8))
+        outfile.write('  #'+bin(code)[2:].zfill(32))
+        outfile.write("\n")
+    
 # check for correct number of arguments
 if len(sys.argv) != 3:
     print("Usage: python assemble.py <input file> <output file>")
@@ -93,36 +167,15 @@ try:
 except:
     print("Error: could not open output file")
     sys.exit()
+
 # read input file into list of lines
 lines = infile.readlines()
 line_num = 0
 
-for line in lines:
-    print("Processing line: " + line)
-    # remove comments
-    line = line.split("#")[0]
-    # remove leading and trailing whitespace
-    line = line.strip()
-    # skip empty lines
-    if line == "":
-        continue
-    # split line into tokens
-    tokens = line.split()
-    # check for instruction
-    instruction = tokens[0]
-    print("Instruction: " + instruction)
-    if instruction not in instruction_names:
-        print("Error: invalid instruction \"" + instruction + "\"" + " on line " + str(line_num))
-        sys.exit()
+collect_labels(lines)
 
-    line_num += 1
+replace_labels(lines)
 
-    check_args(instruction, tokens, line_num)
-    # Create machine code from instruction
-    code = get_machine_code(instruction, tokens)
-    # write instruction opcode to output file in hex of width 8
-    outfile.write('0x'+hex(code)[2:].zfill(8))
-    outfile.write('  #'+bin(code)[2:].zfill(32))
-    outfile.write("\n")
+generate_code(lines, outfile)
 
 
